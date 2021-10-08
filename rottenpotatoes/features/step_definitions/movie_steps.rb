5c2e13591b96862d8b03dc319b6838911f42ddfc
Vicram Rajagopalan <42707603+vicramr@users.noreply.github.com>
2021-10-08 02:24:50 +0000

    completed part 3

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -18,7 +18,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  my_regex = /#{e1}.*#{e2}/
+  expect(my_regex.match?(page.body))
 end
 
 # Make it easier to express checking or unchecking several boxes at once
